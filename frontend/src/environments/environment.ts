634f4f19c374723bfcc8edfef1d7ef39c92e42e3
GhostCode1406 <dachetr@gmail.com>
2026-06-22 14:22:21 +0200

    Fix environment files syntax and values

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl:'https://outworld-invaders-api.onrender.com'
+  production: true,
+  apiUrl: 'https://outworld-invaders-api.onrender.com'
 };
